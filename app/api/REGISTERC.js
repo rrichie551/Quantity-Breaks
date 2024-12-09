c30e69a257323159ec990e0a1b29a7db21251a3b
dev-simpleplan <simple.website.testing@gmail.com>
2024-12-09 18:22:19 +0530

    Changed Commit

--- app/api/REGISTERC.js
+++ app/api/REGISTERC.js
@@ -2,7 +2,7 @@ export const REGISTERC = (functionId) => `mutation {
   discountAutomaticAppCreate(
     automaticAppDiscount: {
       title: "Combo Discount"
-      functionId: "124b9690-da4f-46f1-a887-e94e5aff6278"
+      functionId: "${functionId}"
       startsAt: "2022-06-22T00:00:00"
     }
   ) {
